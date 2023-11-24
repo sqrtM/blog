7ba8bb656cced0b787b5c9baf657fa7b6088af9a
Mason Pike <mpike@flyamelia.com>
2023-11-24 12:06:44 +0100

    cleaned up env and made tests easier to run

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,11 @@ pub struct AppState {
 
 #[tokio::main]
 async fn main() {
+    match dotenvy::dotenv() {
+        Ok(_) => {}
+        Err(_) => {}
+    }
+
     let app_state = AppState {
         db: get_pool().await,
     };
@@ -27,8 +32,41 @@ async fn main() {
         .nest("/api", api_routes())
         .with_state(app_state);
 
-    axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
+    axum::Server::bind(&std::env::var("BIND_ADDR").expect("BIND_ADDR must be set").parse().unwrap())
         .serve(router.into_make_service())
         .await
         .unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use axum::Router;
+    use axum_test::TestServer;
+
+    use crate::AppState;
+    use crate::repositories::get_pool;
+    use crate::routes::{api_routes, routes};
+
+    #[tokio::test]
+    async fn it_should_get() {
+        dotenvy::dotenv().expect("mfw no env");
+
+        let app_state = AppState {
+            db: get_pool().await,
+        };
+
+        let app = Router::new()
+            .nest("/", routes())
+            .nest("/api", api_routes())
+            .with_state(app_state);
+
+
+        let server = TestServer::new(app).unwrap();
+
+        let response = server
+            .get("/users")
+            .await;
+
+        assert_eq!(response.text(), "root user");
+    }
 }
